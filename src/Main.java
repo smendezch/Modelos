12008ccd04b8b935f1ff283c7a49e3d6cbbbe4c7
Saul Mendez Chaverri <smendezch@ucenfotec.ac.cr>
2026-09-17 19:09:52 -0600

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int edad = 20;
+        mensajeCumpleanios(edad); // Paso de parámetro por valor - Se pasa una copia
+        System.out.println(edad);
+
+        ArrayList<String> ejemplo = new ArrayList<>();
+        ejemplo.add("Casa");
+        ejemplo.add("Barco");
+        ejemplo.add("Acero");
+        ordenar(ejemplo); // Paso de parámetro por referencia - Se pasa la dirección de memoria
+        System.out.println(ejemplo);
+    }
+
+    static void mensajeCumpleanios(int edadAnterior) {
+        edadAnterior = edadAnterior + 1;
+        System.out.println("¡Felicidades, cumpliste " + edadAnterior + " años!");
+    }
+
+    static void ordenar(ArrayList<String> original) {
+        original.sort(null);
+        System.out.println("La lista ordenada es: " + original);
+    }
+}
